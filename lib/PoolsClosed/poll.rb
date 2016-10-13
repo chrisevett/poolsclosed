13515768ba02db63335981c285941e35391e6727
chris evett <chris.evett@gmail.com>
2016-10-13 14:37:58 -0400

    green tests and code review. We should refactor these tests

--- lib/PoolsClosed/poll.rb
+++ lib/PoolsClosed/poll.rb
@@ -1,25 +1,34 @@
 require 'celluloid'
 
 module PoolsClosed
+
   # starts a thread which will poll redis for an instance count
   class Poller
+    attr_reader :thread
+  
     def initialize(cnf, machines)
       @cnf = cnf
       @machines = machines
       Systemd::Journal.print(INFO, 'poll starting')
-      @thread = Thread.new { pool_check }
+      @thread = Thread.new { pool_loop }
       sleep 1
     end
 
-    def pool_check
+    # added this so I could test better
+    def pool_loop
       loop do
-        if @machines.quarantine_count < @cnf['quarantine_limit']
-          @machines.add if @machines.pool_count < @cnf['pool_size']
-        else
-          @machines.error('Quarantine count is over the limit')
-        end
-        sleep 5
+        pool_check
+      sleep 5
       end
     end
+
+    def pool_check
+      if @machines.quarantine_count < @cnf['quarantine_limit']
+        @machines.add if @machines.pool_count < @cnf['pool_size']
+      else
+        @machines.error('Quarantine count is over the limit')
+      end
+    end
+
   end
 end
